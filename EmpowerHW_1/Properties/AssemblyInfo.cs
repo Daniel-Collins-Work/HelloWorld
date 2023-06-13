4694db23d592a67397afcdb4852794ca58f84ada
Administrator <Administrator@CRT-DEMO-DMA>
2023-06-13 14:32:38 +0100

    Add project files.

---
+++ EmpowerHW_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHW_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHW_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F6483869-63CC-41CC-8730-525AE66081D2")]
+[assembly: AssemblyVersion("1.0.0.0")]
